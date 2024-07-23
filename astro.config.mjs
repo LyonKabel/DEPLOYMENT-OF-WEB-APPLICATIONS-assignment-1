5b4352e0a949a99b855308971caa27872748e7d5
LyonKabel <93550008+LyonKabel@users.noreply.github.com>
2024-07-23 12:43:04 -0500

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lyonkabel.github.io/DEPLOYMENT-OF-WEB-APPLICATIONS-assignment-1/',
+    site: 'https://lyonkabel.github.io',
+    base: 'DEPLOYMENT-OF-WEB-APPLICATIONS-assignment-1'
 });
